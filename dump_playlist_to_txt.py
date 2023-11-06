57fc1e9a8f83ff99a8fbf6df36c7e71620fd5d47
tbshfr <110125252+tbshfr@users.noreply.github.com>
2023-11-06 21:24:53 +0100

    remove duplicates if song has two artists in db dump

--- dump_playlist_to_txt.py
+++ dump_playlist_to_txt.py
@@ -9,7 +9,7 @@ cursor = conn.cursor()
 
 # SQL query to fetch the required data
 query = """
-SELECT p.name AS playlist_name, s.title AS song_title, a.name AS artist_name
+SELECT p.name AS playlist_name, s.title AS song_title, a.name AS artist_name, spm.position
 FROM Playlist p
 JOIN SongPlaylistMap spm ON p.id = spm.playlistId
 JOIN Song s ON s.id = spm.songId
@@ -24,15 +24,25 @@ cursor.execute(query)
 # Fetch all results
 results = cursor.fetchall()
 
-# Dictionary to hold playlist songs
+# Dictionary to hold playlist songs and artist names
 playlist_songs = {}
 
 # Process the results
-for playlist_name, song_title, artist_name in results:
-    # Format the song entry as "Artist - Song Title" if artist name is present
-    song_entry = f"{artist_name} - {song_title}" if artist_name else song_title
-    # Append the song entry to the corresponding playlist in the dictionary
-    playlist_songs.setdefault(playlist_name, []).append(song_entry)
+for playlist_name, song_title, artist_name, position in results:
+    # Initialize the playlist in the dictionary if it does not exist
+    if playlist_name not in playlist_songs:
+        playlist_songs[playlist_name] = {}
+
+    # Initialize the song in the playlist if it does not exist
+    if song_title not in playlist_songs[playlist_name]:
+        playlist_songs[playlist_name][song_title] = {
+            'artists': set(),  # Use a set to avoid duplicates
+            'position': position
+        }
+
+    # Add the artist name to the set of artists for this song
+    if artist_name:
+        playlist_songs[playlist_name][song_title]['artists'].add(artist_name)
 
 # Folder to save playlist files
 output_folder = "playlists"
@@ -44,10 +54,16 @@ for playlist_name, songs in playlist_songs.items():
     valid_file_name = "".join(x for x in playlist_name if x.isalnum() or x in " -_").rstrip()
     # Path for the file
     file_path = os.path.join(output_folder, f"{valid_file_name}.txt")
+
+    # Sort songs by the position before writing to the file
+    sorted_songs = sorted(songs.items(), key=lambda x: x[1]['position'])
+
     # Create a file for each playlist
     with open(file_path, 'w', encoding='utf-8') as file:
         # Write each song entry to the file
-        for song_entry in songs:
+        for song_title, details in sorted_songs:
+            artist_names = ', '.join(details['artists'])
+            song_entry = f"{artist_names} - {song_title}" if artist_names else song_title
             file.write(song_entry + '\n')
 
 # Close the cursor and connection
